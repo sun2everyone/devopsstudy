98f633e3911ad65d5ec055de1b585a87b0fe5270
sun2everyone <sun2@sun2everyone.ru>
2020-10-15 22:27:15 +0300

    initial packer template created

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
+gpg --export --armor D68FA50FEA312927 | apt-key add -
+bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+apt-get update
+apt install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
+CHK_MONGO_RUNNING=`systemctl status mongod | grep running | wc -l`
+CHK_MONGO_ENABLED=`systemctl status mongod | grep 'service; enabled' | wc -l`
+if [ $CHK_MONGO_RUNNING == "1" ] && [ $CHK_MONGO_ENABLED == "1" ]; then
+ echo "Mongodb installed"
+ exit 0
+else
+ echo "Errors occured!"
+ exit 1
+fi
