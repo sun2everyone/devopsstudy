1d1481721f919426d5a30da76b02b4f9751a4bfb
sun2everyone <sun2@sun2everyone.ru>
2020-10-18 16:49:38 +0300

    moved to terraform-managed ssh firewall rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,3 +65,17 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = "${var.tags}"
 }
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name = "default-allow-ssh"
+  network = "default"
+  priority = 1000
+  description = "Allow to default SSH from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
