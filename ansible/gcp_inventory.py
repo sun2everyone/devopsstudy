de5d38655319960d4310761a910a9344eceee9e9
sun2 <sun2@sun2everyone.ru>
2020-10-22 11:06:39 +0300

    improved gcp_inventory to work with environments

--- ansible/gcp_inventory.py
+++ ansible/gcp_inventory.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import subprocess, sys
+import subprocess, sys, os
 import json
 #check for gcloud 
 inventory={"db":{"hosts":[]},"app":{"hosts":[]},"ungrouped":{"hosts":[]},"_meta": {"hostvars": {}}}
@@ -14,6 +14,17 @@ if not '/gcloud' in gcloud:
     print(json.dumps(inventory))
     sys.exit(0)
 instances = json.loads(subprocess.check_output(["gcloud","compute","instances","list","--format=json"]).decode('utf-8'))
+#check for environment
+script_path=" ".join(sys.argv[:])
+env_mode=False
+if 'environments' in script_path: #if run from environments - use environment mode to get only environment-specific hosts
+    env_mode=True
+    env=""
+    env_pts=script_path.split('/')
+    for  i in range(0,len(env_pts)-1):
+        if env_pts[i]=='environments':
+            env=env_pts[i+1]
+#
 for instance in instances:
     if instance['status']=='RUNNING':
         if len(instance['networkInterfaces']):
@@ -21,6 +32,21 @@ for instance in instances:
                 continue
         else:
             continue
+        #Creating groups for stage-prod-etc and checking for environment. VM must be named as namep1(-name2-...nameN)-environment or namep1(-name2-...nameN)-group
+        parts=instance['name'].split('-')
+        if len(parts)>=2:
+            grp=parts.pop() #assume that last of three is environment|group
+            if not env_mode or (env_mode and grp==env):
+                if grp in inventory.keys():
+                    inventory[grp]['hosts'].append(instance['name'])
+                else:
+                    group={grp:{"hosts":[instance['name']]}}
+                    inventory.update(group)
+            elif env_mode:
+                continue
+        else:
+            if env_mode: #skip if unable to detect environment
+                continue
         if 'natIP' in instance['networkInterfaces'][0]['accessConfigs'][0].keys():
             inventory["_meta"]['hostvars'].update({instance['name']:{"ansible_host":instance['networkInterfaces'][0]['accessConfigs'][0]['natIP']}})
         if '-app' in instance['name']:
@@ -29,14 +55,5 @@ for instance in instances:
             inventory['db']['hosts'].append(instance['name'])
         else:
             inventory['ungrouped']['hosts'].append(instance['name'])
-        #Creating groups for stage-prod-etc
-        parts=instance['name'].split('-')
-        if len(parts)==3:
-            grp=parts[2]
-            if grp in inventory.keys():
-                inventory[grp]['hosts'].append(instance['name'])
-            else:
-                group={grp:{"hosts":[instance['name']]}}
-                inventory.update(group)
 print(json.dumps(inventory))
 
