198177c87a55c5812e1b27e54a5ad6e385bd440d
sun2everyone <sun2@sun2everyone.ru>
2020-11-06 18:38:20 +0300

    docker-3: optimized Dockerfiles to alpine, created readme and run_containers.sh to run reddit microservices app in one command

--- src/README.md
+++ src/README.md
@@ -1,1 +1,28 @@
 # Micorservices
+
+Install and create docker machine (see root README). Switch to remote docker context.
+
+Build commands:
+
+```
+docker build -t reddit/comment:2.2 ./comment
+docker build -t reddit/ui:2.2 ./ui
+docker build -t reddit/post:2.2 ./post-py
+```
+
+Create *terraform.tfvars* with content:
+
+```
+project="gcpprojectname"
+myiprange = "my.external.ip.range/32"
+```
+
+Create firewall rule:
+
+```
+terraform init
+terraform plan
+terraform apply
+```
+
+Run containers in docker-machine using `./run_containers.sh`
--- src/run_containers.sh
+++ src/run_containers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 docker network create reddit
-docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db mongo:latest
-docker run -d --network=reddit --network-alias=post reddit/post:1.0
-docker run -d --network=reddit --network-alias=comment reddit/comment:1.0
-docker run -d --network=reddit -p 9292:9292 reddit/ui:1.0
+docker volume create reddit-db
+docker run --rm -d --network=reddit --network-alias=post_db --network-alias=comment_db -v reddit_db:/data/db --name=reddit-mongo mongo:latest
+docker run --rm -d --network=reddit --network-alias=post --name=reddit-post reddit/post:2.2
+docker run --rm -d --network=reddit --network-alias=comment --name=reddit-comment reddit/comment:2.2
+docker run --rm -d --network=reddit -p 9292:9292 --name=reddit-ui reddit/ui:2.2
+docker-machine ls | grep tcp | sed 's/tcp:/http:/' | sed 's/2376/9292/'
